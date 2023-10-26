b0955c5c41a0474500ce014eddba561ee651227a
Станислав <stasxp3@gmail.com>
2023-10-26 23:19:07 +0200

    Task is finished

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -4,6 +4,6 @@ public class BrokenApp
 {
     public static void main(String []args)
     {
-        System.out.print("fix\nMe");
+        System.out.print("fixMe");
     }
 }
--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -4,6 +4,6 @@ public class UpdatedApp
 {
     public static void main(String[] args)
     {
-        System.out.print("I'm updated can be compiled. And can write to console");
+        System.out.print("I'm updated, can be compiled. And can write to console");
     }
 }
